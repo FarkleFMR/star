a7b11ded67b0403f11cda26dcfa0327191479aa0
FarkleFMR <44482071+FarkleFMR@users.noreply.github.com>
2023-05-26 13:09:37 -0400

    Cambio en el main y y FAT
    Cambio en el main, idea del inicio del create, update y list y finalmente nuevo metodo FAT y una tabla global

--- star.c
+++ star.c
@@ -1,8 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
 
 #define BLOCK_SIZE 32 // Tamaño de bloque en KB
+#define MAX_FILE_NAME_LENGTH 256 // Tamaño máximo del nombre del archivo
 #define TABLE_SIZE 100 // Tamaño máximo de la tabla FAT
+#define MAX_FILE_COUNT 100 // Máxima cantidad de documentos en  un archivo
+
 
 typedef struct {
     int next_block; // Bloque siguiente
@@ -20,6 +25,22 @@ typedef struct {
     int free_blocks_count; // Número de bloques libres
 } FATTable;
 
+FATTable fatTable; // FAT global
+
+void initializeFAT() {
+    // Initialize the FAT table
+    for (int i = 0; i < TABLE_SIZE; i++) {
+        fatTable.entries[i].allocated = 0;
+        fatTable.entries[i].block.next_block = -1;
+    }
+
+    for (int i = 0; i < TABLE_SIZE; i++) {
+        fatTable.free_blocks[i] = i;
+    }
+    fatTable.free_blocks_count = TABLE_SIZE;
+}
+
+
 int getFreeBlock(FATTable *fatTable) {
     if (fatTable->free_blocks_count == 0) {
         return -1; // No hay bloques libres disponibles
@@ -35,51 +56,48 @@ void releaseBlock(FATTable *fatTable, int blockIndex) {
     fatTable->free_blocks[fatTable->free_blocks_count++] = blockIndex;
 }
 
-int main() {
-    FATTable fatTable;
-    int currentBlock = 0; // Bloque actual
-    int totalBlocks = 0; // Número total de bloques utilizados
+void createArchive(){}
+void listArchive(){}
+void updateArchive(){}
 
-    // Inicializar la tabla FAT
-    fatTable.free_blocks_count = TABLE_SIZE;
-    for (int i = 0; i < TABLE_SIZE; i++) {
-        fatTable.entries[i].allocated = 0;
-        fatTable.entries[i].block.next_block = -1;
-        fatTable.free_blocks[i] = i;
+int main(int argc, char* argv[]) {
+    if (argc < 2) {
+        fprintf(stderr, "Usage: %s <options>\n", argv[0]);
+        return 1;
     }
 
-    // Simulación de asignación de bloques
-    int previousBlock = -1;
-    for (int i = 0; i < 10; i++) {
-        // Buscar un bloque libre en la tabla FAT
-        int freeBlock = getFreeBlock(&fatTable);
-        if (freeBlock == -1) {
-            printf("Error: No hay bloques disponibles.\n");
+    const char* option = argv[1];
+
+    if (strcmp(option, "-c") == 0 || strcmp(option, "--create") == 0) {
+        if (argc < 4) {
+            fprintf(stderr, "Usage: %s -c <archive_name> <file1> <file2> ... <fileN>\n", argv[0]);
             return 1;
         }
 
-        // Mostrar información del bloque asignado
-        printf("Bloque asignado: %d\n", freeBlock);
-        printf("Número total de bloques utilizados: %d\n", ++totalBlocks);
-
-        // Enlazar el bloque asignado con el bloque anterior
-        if (previousBlock != -1) {
-            fatTable.entries[previousBlock].block.next_block = freeBlock;
+        initializeFAT();
+        int fileCount = argc - 3;
+        const char** fileList = (const char**)&argv[3];
+        return createArchive(argv[2], fileList, fileCount);
+    } else if (strcmp(option, "-t") == 0 || strcmp(option, "--list") == 0) {
+        if (argc < 3) {
+            fprintf(stderr, "Usage: %s -t <archive_name>\n", argv[0]);
+            return 1;
         }
 
-        // Simular escritura de datos en el bloque
-        // Aquí puedes implementar tu lógica para escribir los datos en el bloque actual
-        // Puedes acceder a los datos a través de fatTable.entries[freeBlock].block.data
-
-        // Actualizar el bloque anterior y pasar al siguiente bloque
-        previousBlock = freeBlock;
-    }
-    for (int i = 0; i < TABLE_SIZE; i++) {
-       if (fatTable.entries[i].allocated) {
-           releaseBlock(&fatTable, i);
+        return listArchive(argv[2]);
+    } else if (strcmp(option, "-u") == 0 || strcmp(option, "--update") == 0) {
+        if (argc < 4) {
+            fprintf(stderr, "Usage: %s -u <archive_name> <file1> <file2> ... <fileN>\n", argv[0]);
+            return 1;
         }
+
+        int fileCount = argc - 3;
+        const char** fileList = (const char**)&argv[3];
+        return updateArchive(argv[2], fileList, fileCount);
+    } else {
+        fprintf(stderr, "Invalid option\n");
+        return 1;
     }
 
-return 0;
-
+    return 0;
 }
