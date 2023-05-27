65ddd339d576651b7e144df2a7e3d9ccf38698f1
FarkleFMR <44482071+FarkleFMR@users.noreply.github.com>
2023-05-26 22:27:11 -0600

    Update star.c
    Create, update, list

--- star.c
+++ star.c
@@ -56,9 +56,188 @@ void releaseBlock(FATTable *fatTable, int blockIndex) {
     fatTable->free_blocks[fatTable->free_blocks_count++] = blockIndex;
 }
 
-void createArchive(){}
-void listArchive(){}
-void updateArchive(){}
+void createArchive(){}int createArchive(const char* archiveName, const char* fileList[], int fileCount) {
+    // Create a new archive
+    FILE* archive = fopen(archiveName, "wb");
+    if (archive == NULL) {
+        fprintf(stderr, "Error creating archive\n");
+        return 1;
+    }
+
+    // Write file count to the archive
+    fwrite(&fileCount, sizeof(int), 1, archive);
+
+    // Process each file and add it to the archive
+    for (int i = 0; i < fileCount; i++) {
+        FILE* file = fopen(fileList[i], "rb");
+        if (file == NULL) {
+            fprintf(stderr, "Error opening file: %s\n", fileList[i]);
+            continue;
+        }
+
+        // Get the file size
+        fseek(file, 0, SEEK_END);
+        long fileSize = ftell(file);
+        fseek(file, 0, SEEK_SET);
+
+        // Write file name length and file name to the archive
+        int fileNameLength = strlen(fileList[i]);
+        fwrite(&fileNameLength, sizeof(int), 1, archive);
+        fwrite(fileList[i], sizeof(char), fileNameLength, archive);
+
+        // Write file size to the archive
+        fwrite(&fileSize, sizeof(long), 1, archive);
+
+        // Write file data to the archive
+        int remainingBytes = fileSize;
+        while (remainingBytes > 0) {
+            int blockIndex = getFreeBlock();
+            if (blockIndex == -1) {
+                fprintf(stderr, "Insufficient space in archive\n");
+                fclose(file);
+                fclose(archive);
+                return 1;
+            }
+
+            Block block;
+            block.next_block = -1;
+
+            int bytesRead = fread(block.data, sizeof(char), BLOCK_SIZE * 1024, file);
+            if (bytesRead <= 0) {
+                break;
+            }
+
+            fwrite(&block, sizeof(Block), 1, archive);
+
+            if (bytesRead < BLOCK_SIZE * 1024) {
+                remainingBytes -= bytesRead;
+                releaseBlock(blockIndex);
+                break;
+            }
+
+            remainingBytes -= bytesRead;
+            fatTable.entries[blockIndex].block.next_block = getFreeBlock();
+        }
+
+        fclose(file);
+    }
+
+    fclose(archive);
+    return 0;
+}
+int listArchive(const char* archiveName) {
+    // List the contents of an archive
+    FILE* archive = fopen(archiveName, "rb");
+    if (archive == NULL) {
+        fprintf(stderr, "Error opening archive\n");
+        return 1;
+    }
+
+    int fileCount;
+    fread(&fileCount, sizeof(int), 1, archive);
+
+    printf("Archive Contents:\n");
+
+    for (int i = 0; i < fileCount; i++) {
+        int fileNameLength;
+        fread(&fileNameLength, sizeof(int), 1, archive);
+
+        char fileName[MAX_FILE_NAME_LENGTH];
+        fread(fileName, sizeof(char), fileNameLength, archive);
+        fileName[fileNameLength] = '\0';
+
+        long fileSize;
+        fread(&fileSize, sizeof(long), 1, archive);
+
+        printf("File Name: %s\n", fileName);
+        printf("File Size: %ld bytes\n", fileSize);
+        printf("\n");
+    }
+
+    fclose(archive);
+    return 0;
+}
+int updateArchive(const char* archiveName, const char* fileList[], int fileCount) {
+    // Update the content of an existing archive
+    FILE* archive = fopen(archiveName, "r+b");
+    if (archive == NULL) {
+        fprintf(stderr, "Error opening archive\n");
+        return 1;
+    }
+
+    // Read the existing file count from the archive
+    int existingFileCount;
+    fread(&existingFileCount, sizeof(int), 1, archive);
+
+    // Calculate the new file count
+    int newFileCount = existingFileCount + fileCount;
+
+    // Update the file count in the archive
+    fseek(archive, 0, SEEK_SET);
+    fwrite(&newFileCount, sizeof(int), 1, archive);
+
+    // Seek to the end of the archive
+    fseek(archive, 0, SEEK_END);
+
+    // Process each file and update it in the archive
+    for (int i = 0; i < fileCount; i++) {
+        FILE* file = fopen(fileList[i], "rb");
+        if (file == NULL) {
+            fprintf(stderr, "Error opening file: %s\n", fileList[i]);
+            continue;
+        }
+
+        // Get the file size
+        fseek(file, 0, SEEK_END);
+        long fileSize = ftell(file);
+        fseek(file, 0, SEEK_SET);
+
+        // Write file name length and file name to the archive
+        int fileNameLength = strlen(fileList[i]);
+        fwrite(&fileNameLength, sizeof(int), 1, archive);
+        fwrite(fileList[i], sizeof(char), fileNameLength, archive);
+
+        // Write file size to the archive
+        fwrite(&fileSize, sizeof(long), 1, archive);
+
+        // Write file data to the archive
+        int remainingBytes = fileSize;
+        while (remainingBytes > 0) {
+            int blockIndex = getFreeBlock();
+            if (blockIndex == -1) {
+                fprintf(stderr, "Insufficient space in archive\n");
+                fclose(file);
+                fclose(archive);
+                return 1;
+            }
+
+            Block block;
+            block.next_block = -1;
+
+            int bytesRead = fread(block.data, sizeof(char), BLOCK_SIZE * 1024, file);
+            if (bytesRead <= 0) {
+                break;
+            }
+
+            fwrite(&block, sizeof(Block), 1, archive);
+
+            if (bytesRead < BLOCK_SIZE * 1024) {
+                remainingBytes -= bytesRead;
+                releaseBlock(blockIndex);
+                break;
+            }
+
+            remainingBytes -= bytesRead;
+            fatTable.entries[blockIndex].block.next_block = getFreeBlock();
+            fatTable.entries[blockIndex].block.next_block = getFreeBlock();
+        }
+
+        fclose(file);
+    }
+
+    fclose(archive);
+    return 0;
+}
 
 int main(int argc, char* argv[]) {
     if (argc < 2) {
