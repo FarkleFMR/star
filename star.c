4590016f5bb3cc263c9f7e0c7f88c1452ebe106f
FarkleFMR <44482071+FarkleFMR@users.noreply.github.com>
2023-05-28 14:33:13 -0600

    Version casi final

--- star.c
+++ star.c
@@ -3,29 +3,30 @@
 #include <string.h>
 #include <unistd.h>
 
-#define BLOCK_SIZE 32 // Tamaño de bloque en KB
-#define MAX_FILE_NAME_LENGTH 256 // Tamaño máximo del nombre del archivo
-#define TABLE_SIZE 100 // Tamaño máximo de la tabla FAT
-#define MAX_FILE_COUNT 100 // Máxima cantidad de documentos en  un archivo
+#define BLOCK_SIZE 32 // Block size in KB
+#define MAX_FILE_NAME_LENGTH 256 // Maximum file name length
 
+#define TABLE_SIZE 100 // Maximum size of the FAT table
+#define MAX_FILE_COUNT 100 // Maximum number of files in an archive
 
 typedef struct {
     int next_block; // Bloque siguiente
+    char filename[256]; // Nombre del archivo
     char data[BLOCK_SIZE * 1024]; // Datos del bloque
 } Block;
 
 typedef struct {
-    int allocated; // Indica si el bloque está asignado (1) o libre (0)
-    Block block; // Bloque de datos
+    int allocated; // Indicates if the block is allocated (1) or free (0)
+    Block block; // Data block
 } FATEntry;
 
 typedef struct {
-    FATEntry entries[TABLE_SIZE]; // Tabla FAT
-    int free_blocks[TABLE_SIZE]; // Índices de bloques libres
-    int free_blocks_count; // Número de bloques libres
+    FATEntry entries[TABLE_SIZE]; // FAT table
+    int free_blocks[TABLE_SIZE]; // Indices of free blocks
+    int free_blocks_count; // Number of free blocks
 } FATTable;
 
-FATTable fatTable; // FAT global
+FATTable fatTable; // Global FAT table
 
 void initializeFAT() {
     // Initialize the FAT table
@@ -41,83 +42,47 @@ void initializeFAT() {
 }
 
 
-int getFreeBlock(FATTable *fatTable) {
-    if (fatTable->free_blocks_count == 0) {
-        return -1; // No hay bloques libres disponibles
+int getFreeBlock() {
+    // Get a free block from the FAT table
+    if (fatTable.free_blocks_count == 0) {
+        return -1; // No free blocks available
     }
 
-    int freeBlockIndex = fatTable->free_blocks[--fatTable->free_blocks_count];
-    fatTable->entries[freeBlockIndex].allocated = 1; // Marcar el bloque como asignado
+    int freeBlockIndex = fatTable.free_blocks[--fatTable.free_blocks_count];
+    fatTable.entries[freeBlockIndex].allocated = 1; // Mark the block as allocated
     return freeBlockIndex;
 }
 
-void releaseBlock(FATTable *fatTable, int blockIndex) {
-    fatTable->entries[blockIndex].allocated = 0; // Marcar el bloque como libre
-    fatTable->free_blocks[fatTable->free_blocks_count++] = blockIndex;
+void releaseBlock(int blockIndex) {
+    // Release a block and mark it as free in the FAT table
+    fatTable.entries[blockIndex].allocated = 0; // Mark the block as free
+    fatTable.free_blocks[fatTable.free_blocks_count++] = blockIndex;
 }
 
-void createArchive(){}int createArchive(const char* archiveName, const char* fileList[], int fileCount) {
-    // Create a new archive
+
+int createArchive(const char* archiveName, const char* fileNames[], int fileCount) {
     FILE* archive = fopen(archiveName, "wb");
     if (archive == NULL) {
         fprintf(stderr, "Error creating archive\n");
         return 1;
     }
 
-    // Write file count to the archive
-    fwrite(&fileCount, sizeof(int), 1, archive);
+    fwrite(&fileCount, sizeof(int), 1, archive); // Write the file count
 
-    // Process each file and add it to the archive
     for (int i = 0; i < fileCount; i++) {
-        FILE* file = fopen(fileList[i], "rb");
+        FILE* file = fopen(fileNames[i], "rb");
         if (file == NULL) {
-            fprintf(stderr, "Error opening file: %s\n", fileList[i]);
-            continue;
+            fprintf(stderr, "Error opening file: %s\n", fileNames[i]);
+            fclose(archive);
+            return 1;
         }
 
-        // Get the file size
-        fseek(file, 0, SEEK_END);
-        long fileSize = ftell(file);
-        fseek(file, 0, SEEK_SET);
+        Block block;
+        strcpy(block.filename, fileNames[i]); // Copy the file name
+        size_t bytesRead = fread(block.data, sizeof(char), sizeof(block.data), file);
+        block.next_block = -1;
 
-        // Write file name length and file name to the archive
-        int fileNameLength = strlen(fileList[i]);
-        fwrite(&fileNameLength, sizeof(int), 1, archive);
-        fwrite(fileList[i], sizeof(char), fileNameLength, archive);
-
-        // Write file size to the archive
-        fwrite(&fileSize, sizeof(long), 1, archive);
-
-        // Write file data to the archive
-        int remainingBytes = fileSize;
-        while (remainingBytes > 0) {
-            int blockIndex = getFreeBlock();
-            if (blockIndex == -1) {
-                fprintf(stderr, "Insufficient space in archive\n");
-                fclose(file);
-                fclose(archive);
-                return 1;
-            }
-
-            Block block;
-            block.next_block = -1;
-
-            int bytesRead = fread(block.data, sizeof(char), BLOCK_SIZE * 1024, file);
-            if (bytesRead <= 0) {
-                break;
-            }
-
-            fwrite(&block, sizeof(Block), 1, archive);
-
-            if (bytesRead < BLOCK_SIZE * 1024) {
-                remainingBytes -= bytesRead;
-                releaseBlock(blockIndex);
-                break;
-            }
-
-            remainingBytes -= bytesRead;
-            fatTable.entries[blockIndex].block.next_block = getFreeBlock();
-        }
+        fwrite(&block, sizeof(Block), 1, archive);
 
         fclose(file);
     }
@@ -125,8 +90,8 @@ void createArchive(){}int createArchive(const char* archiveName, const char* fil
     fclose(archive);
     return 0;
 }
+
 int listArchive(const char* archiveName) {
-    // List the contents of an archive
     FILE* archive = fopen(archiveName, "rb");
     if (archive == NULL) {
         fprintf(stderr, "Error opening archive\n");
@@ -134,103 +99,57 @@ int listArchive(const char* archiveName) {
     }
 
     int fileCount;
-    fread(&fileCount, sizeof(int), 1, archive);
+    fread(&fileCount, sizeof(int), 1, archive); // Read the file count
 
     printf("Archive Contents:\n");
 
     for (int i = 0; i < fileCount; i++) {
-        int fileNameLength;
-        fread(&fileNameLength, sizeof(int), 1, archive);
+        Block block;
+        fread(&block, sizeof(Block), 1, archive);
 
-        char fileName[MAX_FILE_NAME_LENGTH];
-        fread(fileName, sizeof(char), fileNameLength, archive);
-        fileName[fileNameLength] = '\0';
-
-        long fileSize;
-        fread(&fileSize, sizeof(long), 1, archive);
-
-        printf("File Name: %s\n", fileName);
-        printf("File Size: %ld bytes\n", fileSize);
+        printf("File Name: %s\n", block.filename);
+        printf("File Size: %zu bytes\n", strlen(block.data));
         printf("\n");
     }
 
     fclose(archive);
     return 0;
 }
-int updateArchive(const char* archiveName, const char* fileList[], int fileCount) {
-    // Update the content of an existing archive
+
+
+int updateArchive(const char* archiveName, const char* fileNames[], int fileCount) {
     FILE* archive = fopen(archiveName, "r+b");
     if (archive == NULL) {
         fprintf(stderr, "Error opening archive\n");
         return 1;
     }
 
-    // Read the existing file count from the archive
+    // Read the existing file count
     int existingFileCount;
     fread(&existingFileCount, sizeof(int), 1, archive);
 
-    // Calculate the new file count
-    int newFileCount = existingFileCount + fileCount;
-
     // Update the file count in the archive
     fseek(archive, 0, SEEK_SET);
+    int newFileCount = existingFileCount + fileCount;
     fwrite(&newFileCount, sizeof(int), 1, archive);
 
     // Seek to the end of the archive
     fseek(archive, 0, SEEK_END);
 
-    // Process each file and update it in the archive
     for (int i = 0; i < fileCount; i++) {
-        FILE* file = fopen(fileList[i], "rb");
+        FILE* file = fopen(fileNames[i], "rb");
         if (file == NULL) {
-            fprintf(stderr, "Error opening file: %s\n", fileList[i]);
-            continue;
+            fprintf(stderr, "Error opening file: %s\n", fileNames[i]);
+            fclose(archive);
+            return 1;
         }
 
-        // Get the file size
-        fseek(file, 0, SEEK_END);
-        long fileSize = ftell(file);
-        fseek(file, 0, SEEK_SET);
+        Block block;
+        strcpy(block.filename, fileNames[i]); // Copy the file name
+        size_t bytesRead = fread(block.data, sizeof(char), sizeof(block.data), file);
+        block.next_block = -1;
 
-        // Write file name length and file name to the archive
-        int fileNameLength = strlen(fileList[i]);
-        fwrite(&fileNameLength, sizeof(int), 1, archive);
-        fwrite(fileList[i], sizeof(char), fileNameLength, archive);
-
-        // Write file size to the archive
-        fwrite(&fileSize, sizeof(long), 1, archive);
-
-        // Write file data to the archive
-        int remainingBytes = fileSize;
-        while (remainingBytes > 0) {
-            int blockIndex = getFreeBlock();
-            if (blockIndex == -1) {
-                fprintf(stderr, "Insufficient space in archive\n");
-                fclose(file);
-                fclose(archive);
-                return 1;
-            }
-
-            Block block;
-            block.next_block = -1;
-
-            int bytesRead = fread(block.data, sizeof(char), BLOCK_SIZE * 1024, file);
-            if (bytesRead <= 0) {
-                break;
-            }
-
-            fwrite(&block, sizeof(Block), 1, archive);
-
-            if (bytesRead < BLOCK_SIZE * 1024) {
-                remainingBytes -= bytesRead;
-                releaseBlock(blockIndex);
-                break;
-            }
-
-            remainingBytes -= bytesRead;
-            fatTable.entries[blockIndex].block.next_block = getFreeBlock();
-            fatTable.entries[blockIndex].block.next_block = getFreeBlock();
-        }
+        fwrite(&block, sizeof(Block), 1, archive);
 
         fclose(file);
     }
@@ -238,6 +157,7 @@ int updateArchive(const char* archiveName, const char* fileList[], int fileCount
     fclose(archive);
     return 0;
 }
+
 
 int packFromInput(const char* archiveName) {
     // Pack contents from standard input into an archive
